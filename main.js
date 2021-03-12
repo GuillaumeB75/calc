4be7457c41764ef28e4bf02df6713e2dc958d16f
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-03-12 14:25:13 +0100

    -

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const op = require('./operations')
 
 console.log(op.add(10, 11))
@@ -10,3 +11,25 @@ console.log(op.modulo(5, 3))
 //console.log('Hello, Word!!')
 
 console.log(op.myString)
+
+/* calc('*', 10, 10)*/  /*calc(MUL_OP, 10, 10)*/
+
+const readline = require('readline-sync')
+const chalk = require('chalk')
+const calc = require('./calc')
+
+while (true) {
+  //demander l'opération
+  const op = readlineSync.question('Operation:')
+
+  //demander nb1
+  let nb1 = readlineSync.question('nb1: ')
+  nb1 = Number(nb1)
+
+  //demander nb2
+  let nb2 = readlineSync.question('nb2: ')
+  nb2 = Number(nb2)
+
+  console.log(chalk.green.bold(calc(op, nb1, nb2)))
+
+}
