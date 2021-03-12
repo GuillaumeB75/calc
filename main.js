2b50e54e39414ff3ee5623a29ebff604227cac2d
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-03-12 11:01:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const op = require('./operations')
+
+console.log(op.add(10, 11))
+console.log(op.sub(10, 5))
+console.log(op.mul(2, 10))
+console.log(op.div(5, 3))
+console.log(op.modulo(5, 3))
+
+
+//console.log('Hello, Word!!')
+
+console.log(op.myString)
